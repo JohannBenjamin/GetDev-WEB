d68dd57d69b42dd86fbe258ac3b1cff2886e2724
Johann Benjamin <johannduranticona12@gmail.com>
2023-06-15 19:59:34 -0300

    Adicionando o bootstrap e a nova conexão remota

--- Conexão/conexao.php
+++ Conexão/conexao.php
@@ -1,15 +1,27 @@
 <?php 
-$bd = 'GetDev_Web';
-$host = 'localhost';
-$user = 'root';
-$pass = ''; //talvez seja necessario voltar aqui
+$bd = 'pedped668_getdevweb';
+$host = 'mysql.freehostia.com';
+$user = 'pedped668_getdevweb';
+$pass = 'Php_Seguro69'; //talvez seja necessario voltar aqui
 
 
 try {
+    /*$certificado = array(
+        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+    );*/
     $conn = new PDO("mysql:dbname=$bd;host=$host",$user,$pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $conn->exec("set names utf8");
+
+    /*$dsn = "mysql:host={$_host};dbname={$bd}";
+    $options = array(
+        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+    );
+    $pdo = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);*/
 } catch (PDOException $ex) {
     echo $ex->getMessage();
 }
+
+
 ?>
+  
